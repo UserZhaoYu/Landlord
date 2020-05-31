8adef3aaecb49c41667f39786cb1bca0324b96dc
= <=>
2020-06-01 00:03:22 +0800

    优化显示和对象实例化2.0 准备编写手牌函数

--- Landlord/main.cpp
+++ Landlord/main.cpp
@@ -4,9 +4,18 @@ using namespace std;
 
 int main()
 {
-    LandOwner b("张三", "男", "18206061662", "南京", "17张你能秒我？");
-    LandOwner a;
-    a.Info();
-    b.Info();
+//    LandOwner b("张三", "男", "18206061662", "南京", "17张你能秒我？");
+//    LandOwner a;
+//    a.Info();
+//    b.Info();
+    LandOwner * ptr_landOwner = new LandOwner();
+    LandOwner * ptr_landOwner2 = new LandOwner("张三");
+    LandOwner * ptr_landOwner3 = new LandOwner("李四", "男", "18205066166", "南京", "17张你能秒我？");
+    ptr_landOwner3->Info();
+    ptr_landOwner2->Info();
+    ptr_landOwner->Info();
+    delete ptr_landOwner;
+    delete ptr_landOwner2;
+    delete ptr_landOwner3;
     return 0;
 }
--- Landlord/src/LandOwner.cpp
+++ Landlord/src/LandOwner.cpp
@@ -1,20 +1,20 @@
 #include "LandOwner.h"
 
-LandOwner::LandOwner(string name, string sex, string phone, string address, string signature)
+LandOwner::LandOwner(string name, string sex, string phone, string address, string signature):_name(name), _sex(sex), _phone(phone), _address(address), _signature(signature)
 {
-    Set_name(name);
-    Set_sex(sex);
+//    Set_name(name);
+//    Set_sex(sex);
     Set_gold(1000);
     Set_winRate(0);
     Set_victory(0);
     Set_defeat(0);
     Set_experience(0);
-    Set_phone(phone);
-    Set_address(address);
-    Set_signature(signature);
+//    Set_phone(phone);
+//    Set_address(address);
+//    Set_signature(signature);
 
 }
 LandOwner::~LandOwner()
 {
-    //dtor
+    cout << Get_name() << "ÒÑÍË³ö£¡" << endl;
 }
